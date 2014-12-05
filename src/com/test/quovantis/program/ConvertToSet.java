622d9700acb9416608ed66b3a2c401dc0c81f605
avinash <avinash@metadesign.org.in>
2014-12-05 16:06:50 +0530

    Checkin changes

---
+++ src/com/test/quovantis/program/ConvertToSet.java
@@ -0,0 +1,16 @@
+package com.test.quovantis.program;
+
+import java.util.Arrays;
+import java.util.List;
+
+/*
+ * without using any other class Collection
+ */
+public class ConvertToSet {
+
+	public static void main(String... args) {
+		List<Integer> lst = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+				11, 2, 3, 4, 5, 6);
+
+	}
+}
---
+++ src/com/test/quovantis/program/LargeMatrix.java
@@ -0,0 +1,5 @@
+package com.test.quovantis.program;
+
+public class LargeMatrix {
+//http://comproguide.blogspot.in/2013/09/maximum-size-square-sub-matrix-with-all.html
+}
