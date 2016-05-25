99ebc369497f4cdfd215ca87797d0744e9e0a9bd
Avinash Verma <v-avverma@expedia.com>
2016-05-25 13:19:10 +0530

    Shift ds and design pattern code to other repositories

--- src/main/java/com/core/design/pattern/observer/Observer.java
+++
@@ -1,9 +0,0 @@
-package com.core.design.pattern.observer;
-
-//The Observers update method is called when the Subject changes
-
-public interface Observer {
-	
-	public void update(double ibmPrice, double aaplPrice, double googPrice);
-	
-}
